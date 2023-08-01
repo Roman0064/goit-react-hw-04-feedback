e0435ab478162c97cd9ef1599044d7198eb2ef42
Roman0064 <122211211+Roman0064@users.noreply.github.com>
2023-08-01 13:17:30 +0300

    Update Feedback.jsx

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,60 +1,59 @@
-// import {useState} from 'react';
-// import FeedbackOptions from './Feedback.Options/FeedbackOptions'
-// import Statistics from './Statistics/Statistics';
-// import Section from './Section/Section';
-// import Notification from './Notification/Notification';
-// import css from './Feedback.module.css'
+import {useState} from 'react';
+import FeedbackOptions from './Feedback.Options/FeedbackOptions'
+import Statistics from './Statistics/Statistics';
+import Section from './Section/Section';
+import Notification from './Notification/Notification';
+import css from './Feedback.module.css'
 
-// export default const Feedback = () => {
+const Feedback = () => {
   
-//   const [good, setGood] = useState(0);
-//   const [neutral, setNeutral] = useState(0);
-//   const [ bad, setBad] = useState(0);
+  const [ good, setGood] = useState(0);
+  const [ neutral, setNeutral] = useState(0);
+  const [ bad, setBad] = useState(0);
 
-//   const handleFeedback = (type) => {
-//     // this.setState((prevState) => ({
-//     //   ...prevState,
-//     //   [type]: prevState[type] + 1
-//     // }));
-//   };
+  const handleFeedback = (type) => {
+    if (type === 'good') {
+        setGood((prevState) => prevState + 1);
+      } else if (type === 'neutral') {
+        setNeutral((prevState) => prevState + 1);
+      } else if (type === 'bad') {
+        setBad((prevState) => prevState + 1);
+      }
+  };
 
-//   const getCountTotalFeedback = () => {
-//     // const { good, neutral, bad } = this.state;
-//     // return good + neutral + bad;
-    
+  const getCountTotalFeedback = () => good + neutral + bad;
 
-//   };
+  const getCountPositiveFeedBack = () => {
+    const countTotalFeedback = getCountTotalFeedback();
+    return countTotalFeedback === 0 ? 0 : Math.round((good / countTotalFeedback) * 100);
+  };
 
-//   const getCountPositiveFeedBack = () => {
-//     const countTotalFeedback = this.getCountTotalFeedback();
-//     return countTotalFeedback === 0 ? 0 : Math.round((this.state.good / countTotalFeedback) * 100);
-//   };
- 
-//     const { good, neutral, bad } = this.state;
-//     const options = Object.keys(this.state);
-//     const countTotalFeedback = this.getCountTotalFeedback();
-//     const countPositiveFeedbackPercentage = this.getCountPositiveFeedBack();
+  const options = ['good', 'neutral', 'bad'];
+  const countTotalFeedback = getCountTotalFeedback();
+  const countPositiveFeedbackPercentage = getCountPositiveFeedBack();
   
-//     return (
-//       <div className={css.root}>
-//         <Section title="Please leave your feedback">
-//           <FeedbackOptions options={options} onLeaveFeedback={this.handleFeedback} />
-//         </Section>
+    return (
+      <div className={css.root}>
+        <Section title="Please leave your feedback">
+          <FeedbackOptions options={options} onLeaveFeedback={handleFeedback} />
+        </Section>
   
-//         <Section title="Statistics">
-//           {countTotalFeedback === 0 ? (
-//             <Notification message="There is no feedback" />
-//           ) : (
-//             <Statistics 
-//             good={good} 
-//             neutral={neutral} 
-//             bad={bad}
-//             total={countTotalFeedback}
-//             positivePercentage={countPositiveFeedbackPercentage} />
-//           )}
-//         </Section>
-//       </div>
-//     );
-//   };
+        <Section title="Statistics">
+          {countTotalFeedback === 0 ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistics 
+            good={good} 
+            neutral={neutral} 
+            bad={bad}
+            total={countTotalFeedback}
+            positivePercentage={countPositiveFeedbackPercentage} />
+          )}
+        </Section>
+      </div>
+    );
+  };
+
+export default Feedback;
 
 
